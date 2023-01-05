015890475663eea7643a288e0319e29614562fdc
atakan <92584163+AtakanSever@users.noreply.github.com>
2023-01-05 18:25:25 +0300

    Google Maps IOS APIKey edit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\emre\roplanify-client"
+export "FLUTTER_ROOT=/Users/atakansever/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/atakansever/Desktop/flutter_apps/roplanify-client"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=/Users/atakansever/Desktop/flutter_apps/roplanify-client/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/atakansever/Desktop/flutter_apps/roplanify-client/.dart_tool/package_config.json"
